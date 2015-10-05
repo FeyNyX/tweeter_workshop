9048b75767ceadeeff313744226453e34712580f
Wojtek <wojtek.lenartowicz@gmail.com>
2015-10-06 00:54:32 +0200

    Further visual improvements on the main site. It was also merged with show_tweet.php (using Bootstrap's modals).

--- main.php
+++ main.php
@@ -4,11 +4,23 @@
     <meta charset="utf-8">
     <title>Tweeter</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Raleway:500' rel='stylesheet' type='text/css'>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            $('[data-toggle="tooltip"]').tooltip();
+        });
+    </script>
     <style type="text/css">
-        body { background: #f5f8fa !important; }
-        .jumbotron { background: #BFE0EC !important; }
+        body {
+            background: #f5f8fa !important;
+            font-family: 'Raleway', sans-serif !important;
+        }
+        .jumbotron {
+            background-image: url(http://www.pptbackgroundstemplates.com/backgrounds/clouds-illustration-backgrounds.jpg) !important;
+            background-position: bottom !important;
+        }
         hr {
             height: 2px;
             margin-left: auto;
@@ -19,11 +31,24 @@
             margin-top: 35px;
             margin-bottom:15px;
         }
+        #twee {
+            color: white !important;
+        }
+        #comm {
+            color: #31708f !important;
+        }
+        .modal-header {
+            background-color: #337ab7 !important;
+        }
+        #modal-well {
+            font-size: larger;
+        }
     </style>
 </head>
 <body>
 <?php
 require_once("src/connection.php");
+require_once("src/functions.php");
 session_start();
 
 if(isset($_SESSION['user']) == false){
@@ -39,10 +64,10 @@ $myUser = $_SESSION['user'];
             </div>
             <div>
                 <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Home</a></li>
-                    <li><a href="mailbox.php">MailBox</a></li>
+                    <li class="active"><a href="#"><span class='glyphicon glyphicon-home'></span> Home</a></li>
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
+                    <li><a href="mailbox.php"><span class='glyphicon glyphicon-envelope'></span> MailBox</a></li>
                     <li><a href="show_user.php?userId=<?php echo("{$myUser->getId()}") ?>"><span class="glyphicon glyphicon-user"></span> My profile</a></li>
                     <li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
                 </ul>
@@ -63,14 +88,14 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 <div class="container">
     <div class="jumbotron">
         <h1>Tweeter</h1>
-        <p><?php echo("Welcome {$myUser->getEmail()}") ?>!</p>
+        <p><?php echo("Welcome {$myUser->getEmail()}") ?>.</p>
         <hr>
         <h4>What's on your mind?</h4>
         <form role='form' action="main.php" method="post">
             <div class='form-group'>
                 <input class='form-control' id='inputdefault' type="text" name="tweet" placeholder="...">
                 <br>
-                <button class='btn btn-primary' type="submit">Tweet this.</button>
+                <button class='btn btn-primary' type="submit" data-toggle="tooltip" data-placement="bottom" title="Yay!"><span class='glyphicon glyphicon-bullhorn'></span> Tweet this!</button>
             </div>
         </form>
         </div>
@@ -79,39 +104,68 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 
 <?php
 $allTweets = Tweet::loadAllTweets();
+$modalCountdown = count($allTweets);
 echo('<div class="container">
-        <h2>Tweets</h2>
+        <h2>Tweets <span class="badge">' . count($allTweets) . '</span></h2>
         <div class="panel-group">');
 foreach($allTweets as $tweet){
     $tweetComments = Tweet::loadAllCommentsOfTweet($tweet->getId());
     $tweetCreatorId = $tweet->getUserId();
     $tweetCreator = User::getUserById($tweetCreatorId);
+    $tweetCreatorName = substr($tweetCreator->getEmail(), 0, strpos($tweetCreator->getEmail(), "@"));
+    $tweetId = $tweet->getId();
         echo('<div class="panel panel-primary">
                 <div class="panel-heading">');
-    echo("{$tweetCreator->getEmail()} wrote:</div>");
+    echo(time_elapsed_string($tweet->getCreationDate()) . " by <a data-toggle='tooltip' data-placement='right' title='{$tweetCreator->getEmail()}' id='twee' href='show_user.php?userId={$tweetCreatorId}'>{$tweetCreatorName}</a></div>");
                 echo('<div class="panel-body">');
     echo("<div class='well'>{$tweet->getText()}</div>");
-    echo("<button class='btn btn-primary' type='button' a href='show_tweet.php?tweet_id={$tweet->getId()}'>See entire text...</button><hr>");
+    echo("
+        <button type='button' class='btn btn-primary btn-md' data-toggle='modal' data-target='#myModal" . $modalCountdown . "'><span class='glyphicon glyphicon-modal-window'></span> &nbsp;See entire text.</button>
+
+        <div id='myModal" . $modalCountdown . "' class='modal fade' role='dialog'>
+          <div class='modal-dialog'>
+
+            <div class='modal-content'>
+              <div class='modal-header'>
+                <button type='button' class='close' data-dismiss='modal'>&times;</button>
+                <h4 class='modal-title'>Tweet #{$tweet->getId()}</h4>" .
+                time_elapsed_string($tweet->getCreationDate()) . " (" . $tweet->getCreationDate() . ")
+              </div>
+              <div class='modal-body'>
+                <a href='show_user.php?userId={$tweetCreatorId}'>{$tweetCreator->getEmail()}</a> tweeted:
+                <div id='modal-well' class='well'>{$tweet->getText()}</div>
+              </div>
+              <div class='modal-footer'>
+                <button type='button' class='btn btn-info' data-dismiss='modal'>Close</button>
+              </div>
+            </div>
+
+          </div>
+        </div>
+        <hr>
+        ");
+    $modalCountdown--;
     echo("
          <form role='form' action='main.php' method='post'>
             <div class='form-group'>
                 <input class='form-control' id='inputdefault' type='text' name='comment' placeholder='...'>
                 <input type='hidden' name='tweet_id' value='{$tweet->getId()}'>
                 <br>
-                <button class='btn btn-info' type='submit'>Comment this.</button>
+                <button class='btn btn-info' type='submit'><span class='glyphicon glyphicon-comment'></span> &nbsp;Comment this.</button>
             </div>
          </form>
          ");
     if($tweetComments != false) {
         echo('<div class="container">
-                <h4>Comments</h4>
+                <h4>Comments <span class="badge">' . count($tweetComments) . '</span></h4>
                 <div class="panel-group">');
         foreach ($tweetComments as $comment) {
             $commentCreatorId = $comment->getUserId();
             $commentCreator = User::getUserById($commentCreatorId);
+            $commentCreatorName = substr($commentCreator->getEmail(), 0, strpos($commentCreator->getEmail(), "@"));
             echo('<div class="panel panel-info" style="max-width: 40%">
                     <div class="panel-heading">');
-            echo("{$commentCreator->getEmail()} commented:</div>
+            echo("Commented " . time_elapsed_string($comment->getCreationDate()) . " by <a data-toggle='tooltip' data-placement='right' title='{$commentCreator->getEmail()}' id='comm' href='show_user.php?userId={$commentCreatorId}'>{$commentCreatorName}</a></div>
                     <div class='panel-body'>{$comment->getText()}</div></div>");
         }
         echo('</div></div>');
